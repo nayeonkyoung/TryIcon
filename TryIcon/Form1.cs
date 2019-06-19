b0bad36e0de2c831fe4cc4443f4e30eac42adf88
NaYeonKyoung <seteen5087@naver.com>
2019-06-19 11:00:19 +0900

    #2 트레이아이콘 폼 디자인 by 3105 나연경

--- TryIcon/Form1.cs
+++ TryIcon/Form1.cs
@@ -15,5 +15,15 @@ namespace TryIcon
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnTray_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
